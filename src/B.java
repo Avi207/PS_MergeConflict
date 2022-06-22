045291c74671278f567e26408465e49a3eefda1c
avi207 <avinashnit4@gmail.com>
2022-06-22 13:23:21 +0530

    B.java merge conflict is resolved

--- src/B.java
+++ src/B.java
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 This B.java is created by user2
 Now this B.java is modified by user5
 
 This B.java is updated by EMP102-user2
 
+=======
+This B.java is updated by EMP102-user2.
+>>>>>>> 8324951 (B.java is updated)
